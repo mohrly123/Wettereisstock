ad6f06d296ab9c76bd923b12b9a65e069fe27cab
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-12-07 09:24:54 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Wettereisstock/", // YOUR REPO NAME HERE
+  base: "/Wettereisstock/", // Ensure this matches your repository name
 })
